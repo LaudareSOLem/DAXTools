742f3574d107dfbedc1baf6480d9d282f1256059
LaudareSOLem <sven.ontl@outlook.de>
2023-07-29 20:41:38 +0200

    Remaster

--- Import-DAXToolsSettings.ps1
+++ Import-DAXToolsSettings.ps1
@@ -1,3 +1,5 @@
-$xml = [XML](Get-Content (($PSScriptRoot).FullName + '\XML\DAXTools.xml'))
+$XMLFile = $PSScriptRoot + '\XML\DAXTools.xml'
+$xml= [XML] (Get-Content $XMLFile)
+$xml
 $env:Basedir = ($xml.DAXTools.BaseSettings.Basedir)
 $env:DynMgmtUtils = ($xml.DAXTools.BaseSettings.DynMgmtUtils)
